a49b5412749da7b3d2c77add7b2a499ceca4f152
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-07-25 06:04:04 +1200

    Destination Menu works a bit better... But need to make it actually pick a place to explore

---
+++ Areas/scrArea.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+class_name ExplorableArea
+
+@export var Name: String;
+@export var SpawnableEnemies: Array[Character];
--- UI/DestinationMenuUI/scrDestinationMenuUI.gd
+++ UI/DestinationMenuUI/scrDestinationMenuUI.gd
@@ -1,1 +1,14 @@
-extends Node
+extends Control
+
+class_name DestinationMenu;
+@export var Destinations: Array[ExplorableArea];
+@export var ButtonParent: GridContainer;
+var Buttons: Array[Button];
+
+@onready var mButton: PackedScene = preload("res://UI/ButtonUI/nButtonUI.tscn");
+
+func _ready() -> void:
+	for i in Destinations.size():
+		var new_button: Button = mButton.instantiate();
+		ButtonParent.add_child(new_button);
+		new_button.text = Destinations[i].Name;
