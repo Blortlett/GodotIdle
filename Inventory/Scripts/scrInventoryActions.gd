01a966088091a4ba11c86728fdfdc21c8b703147
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-07-27 23:18:12 +1200

    Mouse button release now returns item to last inventory position

--- Inventory/Scripts/scrInventoryActions.gd
+++ Inventory/Scripts/scrInventoryActions.gd
@@ -1,6 +1,8 @@
 class_name InventoryActions extends Control
 # Item drag graphics
 @onready var mDragHandler: ItemDragGfxHandler = get_node("ItemDragGfxHandler");
+var mLastAccessedInvUI: InventoryUI;
+var mLastAccessedID: int;
 # Inventory UI
 @onready var mInventoryGrid: GridContainer = get_node("../InventoryGrid");
 @onready var mInventorySlots: Array[Node] = mInventoryGrid.get_children();
@@ -34,18 +36,36 @@ func OnEquipmentClicked(slot_index: int):
 	# Debug
 	print_debug("Clicked equipment slot index: %d" % slot_index)
 	OnSlotClicked(mEquipmentUI.GetUI(), slot_index)
+
+# Left Mouse Unclicked input here
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.is_released():
+			OnLeftMouseButtonReleased()
+
 #Called by both OnInventoryClicked & OnEquipmentClicked
 func OnSlotClicked(inventoryUI: InventoryUI, slot_index: int):
 	# Drop held Item to slot
 	if mIsItemHeld:
-		mDragHandler.OnItemDropped(inventoryUI.GetInventory().slots[slot_index])
-		inventoryUI.UpdateSlots(); # Update Inventory UI
-		mIsItemHeld = false;
+		DropDraggedItem(inventoryUI, slot_index)
 	# Try Pickup item from slot
 	else:
+		# track where item came from incase want to cancel
+		mLastAccessedInvUI = inventoryUI;
+		mLastAccessedID = slot_index
+		# Get Slot
 		var slot = inventoryUI.GetInventory().slots[slot_index]
 		if slot.amount == 0 or !slot.item:
 			return
 		mDragHandler.OnItemDragged(slot)
 		inventoryUI.UpdateSlots(); # Update Inventory UI
 		mIsItemHeld = true;
+
+func OnLeftMouseButtonReleased():
+	print_debug("Drop Item here :)")
+	DropDraggedItem(mLastAccessedInvUI, mLastAccessedID);
+
+func DropDraggedItem(inventoryUI: InventoryUI, slot_index: int):
+	mDragHandler.OnItemDropped(inventoryUI.GetInventory().slots[slot_index])
+	inventoryUI.UpdateSlots(); # Update Inventory UI
+	mIsItemHeld = false;
