900777944463ed61a5dca18527b9569d24ecd9e4
Blortlett <45956871+Blortlett@users.noreply.github.com>
2025-08-21 18:03:29 +1200

    Added craft button and aligned CraftUI

--- UI/CraftMenuUI/CraftingDescription/scrCraftOutputDescription.gd
+++ UI/CraftMenuUI/CraftingDescription/scrCraftOutputDescription.gd
@@ -2,9 +2,12 @@ class_name CraftOutputDescription extends Node
 @export var mItemNameLabel: Label
 @export var mItemTypeLabel: Label
 @export var mIngredientsParent: GridContainer
+@export var mCraftButton: Button
 
 func _ready() -> void:
 	self.visible = false
+	mCraftButton.text = "CRAFT"
+	mCraftButton.size = Vector2.ZERO
 
 func SetDescription(_recipe: Recipe):
 	mItemNameLabel.text = _recipe.Products[0].Item.name
